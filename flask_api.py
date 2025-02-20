fb29d8e824f8f70ed6190670d3af212679087c90
AzyqanovTalgat <113684036+AzyqanovTalgat@users.noreply.github.com>
2025-02-20 07:10:10 +0500

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -162,5 +162,5 @@ def predict():
     return jsonify({"prediction": prediction})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
